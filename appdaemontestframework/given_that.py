0f0a7335d359fc98fc52f8f893b4ad53490c0182
Tonny Munk Nielsen <Tonnynielsen153@hotmail.com>
2022-10-22 10:00:48 +0200

    First attempt becomes state_of, IsWrapper.

--- appdaemontestframework/given_that.py
+++ appdaemontestframework/given_that.py
@@ -89,6 +89,20 @@ class GivenThatWrapper:
                     'last_updated': last_updated,
                     'last_changed': last_changed
                 }
+            
+            def becomes(self,
+                        state,
+                        attributes=None,
+                        last_updated: datetime = None,
+                        last_changed: datetime = None):
+                if not attributes:
+                    attributes = {}
+                self.mocked_states[entity_id] = {
+                    'main': state,
+                    'attributes': attributes,
+                    'last_updated': last_updated,
+                    'last_changed': last_changed
+                }
 
         return IsWrapper()
 
